d1c38e90a3a5da93e1b567a685536e4d87716c6a
shaik shabintaj <shaikshabintaj9@gmail.com>
2025-12-09 11:59:11 +0530

    print added

--- src/main/java/com/jspiders/Main.java
+++ src/main/java/com/jspiders/Main.java
@@ -3,5 +3,6 @@ package com.jspiders;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world");
+        System.out.println("program strats in dingi branch");
     }
 }
